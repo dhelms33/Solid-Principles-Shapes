13065cdb50eed9fc034f0d727cf055327296a1f2
dhelms33 <derrickhelms10@gmail.com>
2024-08-06 16:33:10 -0600

    Added code and psuedocode for shape and circle java

---
+++ Circle.java
@@ -0,0 +1,19 @@
+public class Shape {
+    public void getArea() {
+    }
+
+    public void getPerimeter() {
+    }
+
+    public void draw() {
+    }
+}
+
+public class Circle extends Shape {
+    private double radius;
+
+    public double getArea(radius) {
+        System.out.println("Area of a circle: ")
+    }
+
+}
---
+++ Shape.md
@@ -0,0 +1,11 @@
+Class Shape
+    method getArea
+    method getPerimeter
+    method getShapeType
+    method getShapeName
+end Class
+
+Class Circle extends Shape
+    method getArea(radius)
+       return pi*3.14*radius*radius
+    method get 
